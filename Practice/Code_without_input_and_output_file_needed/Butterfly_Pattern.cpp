edbf0d74fc0f8e28258db8bef4f37394733f0d5b
ShreyavPratapSingh <kanhaiyapratap1311@gmail.com>
2026-07-17 12:43:55 +0530

    Add files via upload

---
+++ Practice/Code_without_input_and_output_file_needed/Butterfly_Pattern.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,t;
+    cout<<"Enter the number for specific butterfly pattern: ";
+    cin>>n;
+    cout<<endl;
+    t=(2*n)-1;
+    for(int i=1;i<=2*n;i++)
+    {
+       if(i<=n)
+      {
+        for(int j=1;j<=(2*n);j++)
+       {
+        if(j<=((2*n)-t)||j>t)
+        {
+            cout<<"* ";
+        }
+        else if(j>(2*n)-t||j<t)
+        {
+            cout<<"  ";
+        }
+       }
+         cout<<endl;
+         t-=1;
+      }
+    else
+    {
+    for(int j=2*n;j>=1;j--)
+    {
+       if(j<=(2*n)-t-1||j>t+1)
+       {
+            cout<<"* ";
+       }
+       else
+       {
+            cout<<"  ";
+       }
+    }
+    cout<<endl;
+    t+=1;
+  }
+ }
+   return 0;
+}
