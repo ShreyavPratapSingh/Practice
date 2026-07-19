9f7c5d67b3c6ebb9a2a67fe6b21a913caa98c55e
ShreyavPratapSingh <kanhaiyapratap1311@gmail.com>
2026-07-19 21:17:52 +0530

    Add files via upload

---
+++ Practice/Rhombus_Pattern.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+      freopen("input.txt", "r", stdin);
+      freopen("output.txt", "w", stdout);
+    #endif
+    
+    int n,t;
+    cout<<"Enter the number of stars in the side of rhombus: ";
+    cin>>n;
+    cout<<n;
+    cout<<endl;
+    for(int i=1;i<=n;i++)
+    {
+      t=n-i;
+      for(int j=1;j<=n+t;j++)
+      {
+        if(j<=t)
+        {
+          cout<<" ";
+        }
+        else
+        {
+          cout<<"* ";
+        }
+      }
+      cout<<endl;
+    }
+    return 0;
+}
